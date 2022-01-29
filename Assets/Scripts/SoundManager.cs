d33a28764d797ab05000ff317825222c43c62d25
bolam123 <moritz@butenob.net>
2022-01-29 12:03:25 +0100

    SoundManager addeed
    including jump sound Test in player

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -5,15 +5,59 @@ using UnityEngine.Audio;
 
 public class SoundManager : MonoBehaviour
 {
+    public static SoundManager Instance;
     public AudioMixer audioMixer;
 
-    void Start()
+    public GameObject _camera;
+
+    public static string SFX = "SFX";
+    public static string BGM = "BGM";
+    public static string Menu = "Menu";
+
+    private void Awake()
     {
-        
+        Instance = this;       
     }
 
     public void SetLevel (float sliderValue)
     {
         audioMixer.SetFloat("Master_Volume", Mathf.Log10(sliderValue) * 20);
+    }
+
+
+    public AudioSource PlayBGM(AudioClip clip, string audioMixerGroup, bool loop = false, float spatialBlend = 0, float volume = 1)
+    {
+        if(clip == null)return null;
+        return PlayOnGO(clip, _camera, audioMixerGroup, loop, spatialBlend, volume);
+    }
+
+    public AudioSource PlayOnGO(AudioClip clip, GameObject GO, string audioMixerGroup, bool loop = false, float spatialBlend = 0, float volume = 1)
+    {
+        if(clip == null || GO == null)return null;
+
+        AudioSource source = GO.AddComponent<AudioSource>();
+        source.clip = clip;
+        source.loop = loop;
+        source.spatialBlend = spatialBlend;
+        source.volume = volume;
+        AudioMixerGroup[] groups = audioMixer.FindMatchingGroups(audioMixerGroup);
+        if (groups.Length > 0)
+        {
+            source.outputAudioMixerGroup = groups[0];
+        }
+
+        source.Play();
+        if (!loop)
+        {
+            StartCoroutine(DestroySource(clip.length, source));
+        }
+
+        return source;
+    }
+
+    IEnumerator DestroySource(float time, AudioSource source)
+    {
+        yield return new WaitForSeconds(time);
+        Destroy(source);
     }
 }
