a37fc45f1e702483a79ca8aa11c89b6da64f590c
MaFeLP <60669873+MaFeLP@users.noreply.github.com>
2022-01-29 23:39:43 +0100

    Implemented first iteration of the jump mushroom
    Needs different behavior for jumping on it?

---
+++ Assets/Scripts/Interactions/Mushrooms/JumpMushroom.cs
@@ -0,0 +1,20 @@
+using System;
+using UnityEngine;
+
+public class JumpMushroom : MotherMushroom
+{
+    private float _oldGravityScale;
+
+    protected override void StartEffect()
+    {
+        Debug.Log("Applying Effect 'Jump Boost'");
+        _oldGravityScale = PlayerController.playerController.rb.gravityScale;
+        PlayerController.playerController.rb.gravityScale = 0f;
+    }
+
+    protected override void EndEffect()
+    {
+        Debug.Log("Removing Effect 'Jump Boost'");
+        PlayerController.playerController.rb.gravityScale = _oldGravityScale;
+    }
+}
