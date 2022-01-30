8abd08ab958966ffb7429db586cc316adb1553f1
Bennet Rupnow <bennet.rupnow@gym-meiendorf.de>
2022-01-30 17:41:07 +0100

    fix

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -73,7 +73,7 @@ public class GameManager : MonoBehaviour
                 startedReplay = true;
             }
         };
-        // player.GetComponent<SpriteRenderer>().color = Color.red;
+        player.GetComponentInChildren<SpriteRenderer>().color = new Color(1, 1, 1, .6f);
         secondPlayer = Instantiate(playerPrefab);
         secondPlayer.transform.position = startPositionPlayer;
         Camera.Follow = secondPlayer.transform;
