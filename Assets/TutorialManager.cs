26ca861d7af09dc1c1fc4ac72f18da21d1f5fb16
MaFeLP <60669873+MaFeLP@users.noreply.github.com>
2022-01-30 14:40:14 +0100

    Added Tutorial

---
+++ Assets/TutorialManager.cs
@@ -0,0 +1,56 @@
+using TMPro;
+using UnityEngine;
+
+public enum ExplainType
+{
+    KeyPress,
+    Text
+}
+
+public class TutorialManager : MonoBehaviour
+{
+    public GameObject explainCanvas;
+    public TMP_Text before;
+    public TMP_Text after;
+    public TMP_Text key;
+
+    public ExplainType explainType;
+    
+    public string beforeText;
+    public string afterText;
+    public string keyChar;
+
+    public TMP_Text explainTextElement;
+    public string explainTextText;
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        GameObject go = GameManager.Instance.secondRun ?
+            GameManager.Instance.secondPlayer : GameManager.Instance.player;
+
+        if (! col.gameObject.Equals(go)) return;
+
+        explainCanvas.SetActive(true);
+        switch (explainType)
+        {
+            case ExplainType.KeyPress:
+                before.text = beforeText;
+                after.text = afterText;
+                key.text = keyChar;
+                break;
+            case ExplainType.Text:
+                explainTextElement.text = explainTextText;
+                break;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        GameObject go = GameManager.Instance.secondRun ?
+            GameManager.Instance.secondPlayer : GameManager.Instance.player;
+
+        if (! col.gameObject.Equals(go)) return;
+
+        explainCanvas.SetActive(false);
+    }
+}
