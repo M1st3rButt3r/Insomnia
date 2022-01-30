ed67de144cf3c81e9f206fe470d105a42e9e02c6
MaFeLP <60669873+MaFeLP@users.noreply.github.com>
2022-01-30 01:08:37 +0100

    Added Translucent Mushroom Prefab

--- Assets/Scripts/Interactions/Mushrooms/TranslucentMushroom.cs
+++ Assets/Scripts/Interactions/Mushrooms/TranslucentMushroom.cs
@@ -2,17 +2,15 @@
 
 public class TranslucentMushroom : MotherMushroom
 {
-    public GameObject player;
-    
     protected override void StartEffect()
     {
         Debug.Log("Player is now translucent");
-        player.layer = 30;
+        GameManager.Instance.player.layer = 30;
     }
 
     protected override void EndEffect()
     {
         Debug.Log("Player is not translucent anymore");
-        player.layer = 31;
+        GameManager.Instance.player.layer = 31;
     }
 }
